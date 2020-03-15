c2c5e0e74254ea6299cfcaa5752e0c77b541bbe2
Hongyi Zhou <zhouhongyi970826@gmail.com>
2020-03-15 10:13:08 -0400

    more

---
+++ circular_queue.cpp
@@ -0,0 +1,77 @@
+//circular queue
+
+#include <iostream>
+using namespace std;
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+class Queue
+{
+	node *front;
+	node *rear;
+
+public:
+	Queue(){
+		front = NULL;
+		rear = NULL;
+	}
+
+	void createNode(int val)
+	{
+		node *ptr = front;
+		node *nn = new node;
+		nn->data = val;
+		nn->next = NULL;
+		front = nn;
+		rear = nn;
+	}
+
+	void enqueue(int val)
+	{
+		if ( front == NULL || rear == NULL) createNode(val);
+		else{
+			node *ptr = front;
+			node *nn = new node;
+			nn->data = val;
+			rear->next = nn;
+			nn->next = front;
+			rear = nn;
+		}
+	}
+
+	void dequeue()
+	{
+		node *n = front;
+		front = front->next;
+		delete(n);
+	}
+
+	void traverse()
+	{
+		node *ptr = front;
+		do {
+			cout<< ptr->data <<" ";
+			ptr = ptr->next;
+		}while(ptr!=rear->next);
+		cout<<front->data<<endl;
+	}
+};
+
+int main(void)
+{
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60);
+    q.enqueue(70);
+    q.traverse();
+    q.dequeue();
+    q.traverse();
+    return 0;
+}
